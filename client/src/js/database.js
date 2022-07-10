a4a99efc8a6b834f64a287c2f308d9db5b6fd305
April Reitan <45362322+areitan@users.noreply.github.com>
2022-07-10 08:28:44 -0700

    adding database.js code for getDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,11 +14,11 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  console.log('putDb implemented');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, content: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('data saved to the database', result);
 };
@@ -26,18 +26,18 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database. From Activity 28 src js database.js
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  console.log('getDb implemented');
    const contactDb = await openDB('jate', 1);
   const tx = contactDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
